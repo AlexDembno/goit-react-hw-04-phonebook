b4f2b59123cafa355dab096d74b241b38890e1b2
Oleksii Dembnovetskyi <dembnoveckii@gmail.com>
2023-02-03 00:11:16 +0100

    Update FormAddContacts.jsx

--- src/components/FormAddContacts/FormAddContacts.jsx
+++ src/components/FormAddContacts/FormAddContacts.jsx
@@ -67,73 +67,8 @@ function FormAddContacts({ addContacts }) {
     </form>
   );
 }
-// class FormAddContacts extends Component {
-//   state = {
-//     name: '',
-//     number: '',
-//   };
-
-//   handleSubmit = e => {
-//     e.preventDefault();
-//     const { addContacts } = this.props;
-//     addContacts({ ...this.state });
-//     this.reset();
-//   };
-
-//   reset() {
-//     this.setState({
-//       name: '',
-//       number: '',
-//     });
-//   }
-
-//   handleChangeInput = e => {
-//     const { name, value } = e.currentTarget;
-//     this.setState({
-//       [name]: value,
-//     });
-//   };
-//   render() {
-//     const { name, number } = this.state;
-
-//     return (
-//       <form onSubmit={this.handleSubmit} className={style.form}>
-//         <label name="Name">
-//           <input
-//             className={style.input}
-//             type="text"
-//             name="name"
-//             value={name}
-//             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-//             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-//             required
-//             placeholder="Enter name"
-//             onChange={this.handleChangeInput}
-//           />
-//         </label>
-//         <label name="number">
-//           <input
-//             className={style.input}
-//             type="tel"
-//             name="number"
-//             value={number}
-//             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-//             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-//             required
-//             placeholder="Enter number"
-//             onChange={this.handleChangeInput}
-//           />
-//         </label>
-//         <button className={style.button} type="submit">
-//           Add contact
-//         </button>
-//       </form>
-//     );
-//   }
-// }
 
 export default FormAddContacts;
-
 FormAddContacts.propTypes = {
   addContacts: PropTypes.func.isRequired,
 };
